213e71052834f0d13d77060ea6ba2e5ba4c3317b
ksaidivya@teksystems.com <ksaidivya@teksystems.com>
2021-10-18 12:29:46 +0530

    second commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,18 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CheckOutComponent } from './check-out/check-out.component';
-import { ProgressBarComponent } from './progress-bar/progress-bar.component';
-import { HomeInsuranceComponent } from './home-insurance/home-insurance.component';
-import { PaymentComponent } from './payment/payment.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     CheckOutComponent,
-    ProgressBarComponent,
-    HomeInsuranceComponent,
-    PaymentComponent
-  ],
+      ],
   imports: [
     BrowserModule,
     AppRoutingModule

ce03fca11d424cd49fdfe82f5ce2baaf07cb6098
ksaidivya@teksystems.com <ksaidivya@teksystems.com>
2021-10-18 11:33:26 +0530

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,9 @@
 import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CheckOutComponent } from './check-out/check-out.component';
-import { NextComponent } from './next/next.component';
 
 const routes: Routes = [
   {path:'check-out' , component :CheckOutComponent},
-  {path:'next' , component :NextComponent},
   {path:'' ,pathMatch:'full', redirectTo :'/check-out'},
 
 
